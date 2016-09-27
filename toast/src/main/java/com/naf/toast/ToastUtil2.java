9671bd3164450e2636721d402b4742765cdcd763
xuwab <xuwab@outlook.com>
2016-09-27 21:35:06 +0800

    wrote a new class ToastUtil by Thread from Hq

---
+++ toast/src/main/java/com/naf/toast/ToastUtil2.java
@@ -0,0 +1,66 @@
+package com.naf.toast;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2016/9/27.
+ */
+
+public class ToastUtil2 {
+    public static final int LENGTH_SHORT = 0;
+    public static final int LENGTH_LONG = 1;
+    private static Handler handler = new Handler(Looper.getMainLooper());
+    private static Toast toast = null;
+    private static Object synObj = new Object();//你不知道是什么类
+
+    public static void makeText(final Context act, final String msg,
+                                   final int len) {
+        new Thread(new Runnable() {
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        synchronized (synObj) {
+                            if (toast != null) {
+                                toast.cancel();
+                                toast.setText(msg);
+                                toast.setDuration(len);
+                            } else {
+                                toast = Toast.makeText(act, msg, len);
+                            }
+                            toast.show();
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+    public static void makeText(final Context act, final int msg,
+                                   final int len) {
+        new Thread(new Runnable() {
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        synchronized (synObj) {
+                            if (toast != null) {
+                                toast.cancel();
+                                toast.setText(msg);
+                                toast.setDuration(len);
+                            } else {
+                                toast = Toast.makeText(act, msg, len);
+                            }
+                            toast.show();
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+
+}
